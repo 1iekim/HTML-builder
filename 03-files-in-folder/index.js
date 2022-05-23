9553a75d1b97b1d1e515e8a4a63084efcadb64ba
Mikeil Akovity (1iekim) <mikeil98@mail.ru>
2022-05-23 23:50:21 +0300

    feat: add solution for 03-files-in-folder (Mon, May 23, 2022 11:50 PM)

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const { readdir } = require('fs/promises');
+const path = require('path');
+const way = path.join(__dirname, 'secret-folder');
+
+async function redProm() {
+  const files = await readdir(way, {withFileTypes: true});
+  const arrFile = [];  
+  
+  for (let file of files) {
+    if(file.isFile()){
+      arrFile.push(file.name);
+    }
+  }
+
+  for (let file of files) {
+    if(!file.isFile()) continue;
+    
+    let timeWay = path.join(way, file.name); 
+    
+    fs.stat(timeWay, (err, st) => {
+      let fullname = arrFile[arrFile.indexOf(file.name)];
+      let name = fullname.slice(0,-4);
+      let dirent = fullname.slice(file.name.lastIndexOf('.')+1);
+    
+      console.log(`${name} - ${dirent} - ${st.size}`);
+    });
+  }
+}
+
+redProm();
