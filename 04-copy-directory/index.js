398a48b6ea1002cd4909d20e523f6d48f2d39792
Mikeil Akovity (1iekim) <mikeil98@mail.ru>
2022-05-23 23:51:08 +0300

    feat: add solution for 04-copy-directory (Mon, May 23, 2022 11:51 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 
+const way = path.join(__dirname, 'files');
+const wayCopy = path.join(__dirname, 'files-copy');
+
+fs.mkdir(wayCopy, {recursive: true}, (err)=>{
+  if(err) console.log('Err', err.message);
+});
+
+fs.readdir(way, (err, files) => {
+  files.forEach(file => {
+    fs.copyFile(path.join(way, file), path.join(wayCopy, file), () => {});
+  });
+})
